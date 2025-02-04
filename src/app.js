471ff73eb8d6a6a0ae9286f1959053614744c79c
arinostechsolutions <consultoria.afonsocruz@gmail.com>
2025-02-04 11:04:52 -0300

    changed to port 8080

--- src/app.js
+++ src/app.js
@@ -21,7 +21,7 @@ app.use(cors());
 app.use("/api/reports", reportRoutes);
 app.use("/api/cities", cityRoutes);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
 });
